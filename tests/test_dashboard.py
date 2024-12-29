7be3edb94e2205c10efc9ee5e18862fd0f067e77
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2024-12-28 22:58:15 -0500

    Added test to batch messages

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -2,7 +2,7 @@ import pytest
 
 from dashboard import get_ursim_ip
 from dashboard.datatypes import DashboardMessages
-from dashboard.dashboard import check_connection, send_message
+from dashboard.dashboard import check_connection, send_message, send_batch_messages
 
 
 ip = get_ursim_ip()
@@ -18,3 +18,16 @@ async def test_connection():
 async def test_communication():
     r = await send_message(ip, DashboardMessages.robot_mode)
     assert r.split(b":")[0] == b"Robotmode"
+
+
+@pytest.mark.asyncio
+async def test_batch_messages():
+    messages = [
+        DashboardMessages.robot_mode,
+        DashboardMessages.is_program_saved,
+        DashboardMessages.running,
+    ]
+    r = await send_batch_messages(ip, messages)
+    assert isinstance(r, list)
+    assert isinstance(r[0], tuple)
+    assert r[0][1].split(b":")[0] == b"Robotmode"
