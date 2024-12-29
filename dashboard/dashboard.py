7c57280fcd63cb764052838d0de429c651db8da5
lals-m3 <16458080+lalondesteve@users.noreply.github.com>
2024-12-28 22:28:56 -0500

    First tests passed, added dashboard batch command

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,6 +5,7 @@ from .datatypes import DashboardMessages
 
 logger = logging.getLogger(__name__)
 PORT = 29999
+_END = b"\r\n"
 
 
 async def check_connection(ip):
@@ -20,20 +21,36 @@ async def check_connection(ip):
 
 async def send_message(ip: str, message: DashboardMessages, value: str = ""):
     w = None
-    _end = b"\r\n"
     _value = (" " + value + " ").encode() if value else b""
     try:
         r, w = await asyncio.wait_for(asyncio.open_connection(ip, PORT), 2)
         buf = await r.read(1024)
         if b"Connected" in buf:
-            w.write(message.encode() + _value + _end)
+            w.write(message.encode() + _value + _END)
             buf = await r.read(1024)
         else:
-            raise ConnectionError
+            raise ConnectionError(buf)
     finally:
         if w:
             w.close()
     return buf.strip()
+
+
+async def send_batch_messages(ip: str, messages: list[DashboardMessages]):
+    w = None
+    responses: list[tuple[DashboardMessages, bytes]] = []
+    try:
+        r, w = await asyncio.wait_for(asyncio.open_connection(ip, PORT), 2)
+        buf = await r.read(1024)
+        if b"Connected" not in buf:
+            raise ConnectionError(buf)
+        for m in messages:
+            w.write(m.encode() + _END)
+            responses.append((m, await r.read(2048)))
+    finally:
+        if w:
+            w.close()
+    return responses
 
 
 if __name__ == "__main__":
@@ -42,5 +59,20 @@ if __name__ == "__main__":
         ip = get_ursim_ip()
         print(await check_connection(ip))
         print(await send_message(ip, DashboardMessages.robot_mode))
+        # messages = [
+        #     DashboardMessages.get_loaded_program,
+        #     DashboardMessages.robot_mode,
+        #     DashboardMessages.is_program_saved,
+        #     DashboardMessages.running,
+        #     DashboardMessages.safety_status,
+        # ]
+        from pprint import pprint
+
+        pprint(
+            await send_batch_messages(
+                ip,
+                [m for m in DashboardMessages if m is not DashboardMessages.shutdown],
+            )
+        )
 
     asyncio.run(run())
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,3 +1,5 @@
+import pytest
+
 from dashboard import get_ursim_ip
 from dashboard.datatypes import DashboardMessages
 from dashboard.dashboard import check_connection, send_message
@@ -6,11 +8,13 @@ from dashboard.dashboard import check_connection, send_message
 ip = get_ursim_ip()
 
 
+@pytest.mark.asyncio
 async def test_connection():
     r = await check_connection(ip)
     assert r.split(b":")[0] == b"Connected"
 
 
+@pytest.mark.asyncio
 async def test_communication():
     r = await send_message(ip, DashboardMessages.robot_mode)
     assert r.split(b":")[0] == b"Robotmode"
