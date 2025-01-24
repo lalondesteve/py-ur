92472a46d94a051675428630ab60f6acf0759946
Steve Arch <steve@arch>
2025-01-24 12:14:42 -0500

    upgraded deps + fixed __init__

--- py_ur/__init__.py
+++ py_ur/__init__.py
@@ -1,6 +1,4 @@
-import py_ur.dashboard as dashboard
-import py_ur.modbus as modbus
 from .robot import Robot
 from .utils import get_ursim_ip
 
-__all__ = ["Robot", "dashboard", "get_ursim_ip", "modbus"]
+__all__ = ["Robot", "get_ursim_ip"]
